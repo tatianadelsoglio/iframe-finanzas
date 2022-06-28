e528ebe9411ccd885f2484409378e58c3a1f8390
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-06-28 11:51:39 -0300

    se renombro la carpeta cardFinanzas a Finanzas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import Finanzas from "./components/ui/cardsFinanazas/Finanzas";
+import Finanzas from "./components/ui/finanazas/Finanzas";
 
 
 const App = () => {
