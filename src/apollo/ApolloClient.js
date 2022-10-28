a153a47bf1873bd34987ee6dc0a5205877878f84
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-10-28 11:26:09 -0300

    se cambio uri

--- src/apollo/ApolloClient.js
+++ src/apollo/ApolloClient.js
@@ -13,8 +13,8 @@ export const client = new ApolloClient({
     // uri: "http://170.239.49.41:4002",
     //uri: "http://beeapp.binamics.com.ar:4002",
     // uri: "http://localhost:4002",
-     //uri: URL,
-    uri: "http://10.0.0.28:4002",
-    // uri: "http://170.239.49.41:4002",
+     uri: URL,
+    ///uri: "http://10.0.0.28:4002",
+    //uri: "http://170.239.49.41:4002",
   }),
 });
